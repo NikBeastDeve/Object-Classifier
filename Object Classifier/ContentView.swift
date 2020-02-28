b5fe8f1418d0b79cf2c36a54c0481fcfb0d1fccd
Nikita Galaganov <nickgalaganov@gmail.com>
2020-02-28 15:35:51 +0100

    Initial Commit

---
+++ Object Classifier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Object Classifier
+//
+//  Created by Nikita Galaganov on 28.02.2020.
+//  Copyright © 2020 Nikita Galaganov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
